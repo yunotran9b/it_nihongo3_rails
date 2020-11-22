3ed1cc3a16f19da9c64376f2effb73724ab0ad0f
Ubuntu <ubuntu@ip-172-31-32-231.ec2.internal>
2020-11-22 14:35:46 +0000

    Transition to book

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/',to: 'static_pages#home'
+  get '/',to: 'static_pages#home', as: 'home'
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
